78e406cea20abda79601da367a65660d7d77186d
davy <davy.wybiral@gmail.com>
2018-08-23 09:47:59 -0500

    add cli usage & version

--- cmd/hub/main.go
+++ cmd/hub/main.go
@@ -4,11 +4,19 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/wybiral/hub/internal/app"
 )
 
+// Current hub version
+const version = "0.1.0"
+
 func main() {
+	flag.Usage = func() {
+		usage()
+		os.Exit(0)
+	}
 	var host string
 	flag.StringVar(&host, "h", "127.0.0.1", "Server Host")
 	var port int
@@ -24,4 +32,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func usage() {
+	fmt.Print("NAME:\n")
+	fmt.Print("   hub\n\n")
+	fmt.Print("USAGE:\n")
+	fmt.Print("   hub -h 127.0.0.1 -p 8080 [-d /public/directory]\n\n")
+	fmt.Print("VERSION:\n")
+	fmt.Printf("   %s\n\n", version)
 }
